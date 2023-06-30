1544219fe3f98077f063dae8846747ecd4c52607
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-06-30 22:58:36 +1000

    Dev

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a Python package containing the Model and Param functions files, as well as the a file for calling the load functions when provided with a JSON representation of the fridge.
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+#For creating the package
+#Imports numpy, pandas, json
+from setuptools import setup
+setup(
+    name='Cryo_UTS',
+    packages=['Cryo_UTS'],
+    description='For modelling heat and noise loads',
+    version='0.1',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'json'
+    ]
+)
